7227b374f8074e4a79df3c6593cb7061464b66d0
JJM08155 <geumman45@gmail.com>
2024-11-08 01:14:45 +0900

    feat: 기능생성 메세지

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # teamproject_2024
 ## 기초프로젝트2 팀 기말 대체 프로젝트
 
+teampro.java를 이용하시면 됩니다.
---
+++ teampro.java
@@ -0,0 +1,7 @@
+public class teampro {
+    public static void main(String[] args) {
+        System.err.println("안녕하세요");
+    }
+
+
+}
